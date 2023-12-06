561f94966235adefd935b602ebb01efa1157ae1a
HolbertonFalcon <144282143+HolbertonFalcon@users.noreply.github.com>
2023-12-06 11:34:23 -0600

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,58 @@
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+/* int main(int ac, char **av)*/
+
+int check_path(char *path, char **pathArray, char **tokenArray);
+int main(int argc, char **argv)
+{
+        char *input = NULL, *path = NULL;
+        size_t size = 0;
+        char *tokenArray[20], *pathArray[20];
+        int ret_value = 0, i;
+        (void)argc;
+        (void)argv;
+        while (1)
+        {
+                i = 0;
+                while (environ[i] != NULL)
+                {
+                        if (strncmp(environ[i], "PATH=", 5) == 0)
+                        {
+                                path = strdup((environ[i] + 5));
+                                break;
+                        }
+                        i++;
+                }
+                tokenize_string(path, ":", pathArray);
+                if (isatty(STDIN_FILENO))
+                        write(STDOUT_FILENO, "Shell $ ", 8);
+                if (getline(&input, &size, stdin) == -1)
+                {
+                        free(input);
+                        free(path);
+                        exit(EXIT_SUCCESS);
+                }
+                tokenize_string(input, " \n\t", tokenArray);
+                if (!tokenArray[0])
+                {
+                        free(path);
+                        continue;
+                }
+                if (strcmp(tokenArray[0], "exit") == 0)
+                {
+                        free(input);
+                        free(path);
+                        exit(EXIT_SUCCESS);
+                }
+                if (access(tokenArray[0], X_OK) == 0)
+                        execute(path, tokenArray[0], tokenArray);
+                else
+                        ret_value = check_path(path, pathArray, tokenArray);
+        }
+        return (ret_value);
+}
+
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
