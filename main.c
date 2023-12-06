06ad65e32b1c0d6e88845e878b56f8f0f4aa925e
HolbertonFalcon <144282143+HolbertonFalcon@users.noreply.github.com>
2023-12-06 12:03:16 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+*#include "shell.h"
 
 /**
  * main - entry point
@@ -43,53 +43,58 @@
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }*/
-/*#include "main.h"*/
-int check_path(char *path, char **pathArray, char **tokenArray);
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-        char *input = NULL, *path = NULL;
-        size_t size = 0;
-        char *tokenArray[20], *pathArray[20];
-        int ret_value = 0, i;
-        (void)argc;
-        (void)argv;
-        while (1)
+    info_t info[] = { INFO_INIT };
+    int fd = 2;
+
+    asm ("mov %1, %0\n\t"
+        "add $3, %0"
+        : "=r" (fd)
+        : "r" (fd));
+
+    if (ac == 2)
+    {
+        fd = open(av[1], O_RDONLY);
+        if (fd == -1)
         {
-                i = 0;
-                while (environ[i] != NULL)
-                {
-                        if (strncmp(environ[i], "PATH=", 5) == 0)
-                        {
-                                path = strdup((environ[i] + 5));
-                                break;
-                        }
-                        i++;
-                }
-                tokenize_string(path, ":", pathArray);
-                if (isatty(STDIN_FILENO))
-                        write(STDOUT_FILENO, "Shell $ ", 8);
-                if (getline(&input, &size, stdin) == -1)
-                {
-                        free(input);
-                        free(path);
-                        exit(EXIT_SUCCESS);
-                }
-                tokenize_string(input, " \n\t", tokenArray);
-                if (!tokenArray[0])
-                {
-                        free(path);
-                        continue;
-                }
-                if (strcmp(tokenArray[0], "exit") == 0)
-                {
-                        free(input);
-                        free(path);
-                        exit(EXIT_SUCCESS);
-                }
-                if (access(tokenArray[0], X_OK) == 0)
-                        execute(path, tokenArray[0], tokenArray);
-                else
-                        ret_value = check_path(path, pathArray, tokenArray);
+            if (errno == EACCES)
+                exit(126);
+            if (errno == ENOENT)
+            {
+                _eputs(av[0]);
+                _eputs(": 0: Can't open ");
+                _eputs(av[1]);
+                _eputchar('\n');
+                _eputchar(BUF_FLUSH);
+                exit(127);
+            }
+            return (EXIT_FAILURE);
         }
-        return (ret_value);
+        info->readfd = fd;
+    }
+
+    // Redirect stdout to a file descriptor (e.g., a pipe)
+    int saved_stdout = dup(STDOUT_FILENO);
+    int stdout_pipe[2];
+    pipe(stdout_pipe);
+    dup2(stdout_pipe[1], STDOUT_FILENO);
+    close(stdout_pipe[1]);
+
+    populate_env_list(info);
+    read_history(info);
+    hsh(info, av);
+
+    // Restore original stdout
+    dup2(saved_stdout, STDOUT_FILENO);
+    close(saved_stdout);
+
+    // Print the contents of stdout_pipe[0]
+    char buffer[4096];
+    ssize_t bytesRead;
+    while ((bytesRead = read(stdout_pipe[0], buffer, sizeof(buffer))) > 0) {
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
+
+    return (EXIT_SUCCESS);
 }
