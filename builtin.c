a4c8e44f2fa4eb0e229a96dc56058fe7909e0165
HolbertonFalcon <144282143+HolbertonFalcon@users.noreply.github.com>
2023-12-07 12:05:21 -0600

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,17 +1,15 @@
 #include "shell.h"
 
 /**
- * _myexit - closes the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          consistent function prototype.
- *  Return: closes with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * _handle_exit_arg - handles the exit argument
+ * @info: Structure containing potential arguments.
+ * Return: 1 on error, -2 on success
  */
-int _myexit(info_t *info)
+static int _handle_exit_arg(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	if (info->argv[1])
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -20,85 +18,103 @@ int _myexit(info_t *info)
 			print_error(info, "Illegal number: ");
 			_eputs(info->argv[1]);
 			_eputchar('\n');
-			return (1);
+			return 1;
 		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
+		info->err_num = exitcheck;
+		return -2;
 	}
+
 	info->err_num = -1;
-	return (-2);
+	return -2;
+}
+
+/**
+ * _myexit - closes the shell
+ * @info: Structure containing potential arguments.
+ * Return: closes with a given exit status (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	return _handle_exit_arg(info);
+}
+
+/**
+ * _change_directory - changes the current folder of the process
+ * @info: Structure containing potential arguments.
+ * @new_dir: Directory to change to.
+ * Return: 0 on success, 1 on error
+ */
+static int _change_directory(info_t *info, const char *new_dir)
+{
+	char buffer[1024];
+	int chdir_ret;
+
+	chdir_ret = chdir(new_dir);
+
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(new_dir), _eputchar('\n');
+		return 1;
+	}
+
+	_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+	_setenv(info, "PWD", getcwd(buffer, 1024));
+
+	return 0;
 }
 
 /**
  * _mycd - changes the current folder of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          consistent function prototype.
- *  Return: Always 0
+ * @info: Structure containing potential arguments.
+ * Return: Always 0
  */
 int _mycd(info_t *info)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+	char *dir, *home_dir;
 
 	/* Get the current working directory */
-	s = getcwd(buffer, 1024);
-	if (!s)
+	char *current_dir = getcwd(NULL, 0);
+	if (!current_dir)
 		_puts("TODO: Handle getcwd failure\n");
 
 	/* If no argument provided, change to the home directory */
 	if (!info->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
+		home_dir = _getenv(info, "HOME=");
+		dir = home_dir ? home_dir : _getenv(info, "PWD=");
+		_change_directory(info, dir ? dir : "/");
 	}
 	/* If the argument is "-", change to the previous directory (OLDPWD) */
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-		if (!_getenv(info, "OLDPWD="))
+		dir = _getenv(info, "OLDPWD=");
+		if (!dir)
 		{
-			_puts(s);
+			_puts(current_dir);
 			_putchar('\n');
-			return (1);
+			return 1;
 		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+
+		_puts(dir), _putchar('\n');
+		_change_directory(info, dir);
 	}
 	/* Change to the specified directory */
 	else
-		chdir_ret = chdir(info->argv[1]);
+		_change_directory(info, info->argv[1]);
 
-	/* Handle errors if chdir fails */
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	/* Update environment variables (OLDPWD and PWD) on success */
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-
-	return (0);
+	free(current_dir);
+	return 0;
 }
 
 /**
- * _myhelp - changes the current folder of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          consistent function prototype.
- *  Return: Always 0
+ * _myhelp - displays help information
+ * @info: Structure containing potential arguments.
+ * Return: Always 0
  */
 int _myhelp(info_t *info)
 {
-	char **arg_array;
-
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
+	(void)info;
+	_puts("help call works. Function not yet implemented\n");
+	return 0;
 }
